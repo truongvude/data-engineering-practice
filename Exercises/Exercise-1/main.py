d7fd1cf28fa31692400511834a744db173bd9f12
truongvude <truongvude@outlook.com>
2025-07-22 01:02:15 +0700

    Update exercise 1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 download_uris = [
@@ -10,11 +11,30 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+current_folder = os.path.dirname(__file__)
+download_folder = os.path.join(current_folder, "downloads")
 
 def main():
-    # your code here
-    pass
+    if not os.path.exists(download_folder):
+       os.makedirs(download_folder)
 
+    for url in download_uris:
+        try:
+            response = requests.get(url)
+            print(response.raise_for_status())
+            file_name = os.path.basename(url)
+            file_path = os.path.join(download_folder, file_name)
+
+            with open(file_path, "wb") as f:
+                f.write(response.content)
+            os.system(f"unzip -qq {file_path} -d {download_folder}")
+            print(f"Successfully download and extract file {file_name}")
+            os.system(f"rm -rf {file_path}")
+            print(f"Deleted file {file_name}")
+
+        except Exception as e:
+            print(f"Error: {e}")
+        
 
 if __name__ == "__main__":
     main()
